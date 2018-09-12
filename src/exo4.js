11ce00cd5d594a130e0bc57e8f6992e1a0f9a0f3
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-09-12 11:34:14 +0200

    exo 4

---
+++ src/exo4.js
@@ -0,0 +1,13 @@
+export function addWatcher(obj, prop, getter, setter) {
+  // change le descripteur pour appeler les fonctions:
+  // - getter lorsque la propriété prop est accédée
+  // - setter lorsque la propriété prop est modifiée
+  return obj;
+}
+
+export function setPrivatesAndConstants(obj) {
+  // changer le descripteur des propriétés:
+  // - commençant par un _ : non énumérables
+  // - commençant par une majuscule : non mutables et non configurables
+  return obj;
+}
