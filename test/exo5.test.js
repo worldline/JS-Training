63b3d556faba60c65054a70ed68d2a5c1f774cef
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-09-12 18:21:09 +0200

    exo 6

---
+++ test/exo5.test.js
@@ -0,0 +1,13 @@
+import { bob, User, Person } from "../src/exo5";
+
+it("should have User prototype", () => {
+  expect(Object.getPrototypeOf(bob)).toBe(User);
+  expect(bob.hasRight("create")).toBe(true);
+  expect(bob.hasRight("delete")).toBe(false);
+});
+
+it("should have Person prototype", () => {
+  expect(Object.getPrototypeOf(User)).toBe(Person);
+  expect(Person.isPrototypeOf(bob)).toBe(true);
+  expect(bob.getFullName()).toBe("Bob Afett");
+});
