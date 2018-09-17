ab39b25abf4dd7b300f70fe6a83bacd18a81bdb1
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-09-17 16:59:28 +0200

    fixes exo 8

--- test/exo8.test.js
+++ test/exo8.test.js
@@ -2,11 +2,9 @@ import { parseUserData } from "../src/exo8";
 
 const a = parseUserData({
   // retourner data.name ou "Anonymous" si data.name est falsey
-  //name: data.name,
   name: "Luke",
 
   // convertir data.age en nombre, retourner 0 si ce n'est pas un nombre
-  //age: data.age,
   age: 28,
 
   // retourner true si data.rightId est égal à 0
@@ -19,11 +17,9 @@ const a = parseUserData({
 
 const b = parseUserData({
   // retourner data.name ou "Anonymous" si data.name est falsey
-  //name: data.name,
   name: "",
 
   // convertir data.age en nombre, retourner 0 si ce n'est pas un nombre
-  //age: data.age,
   age: "",
 
   // retourner true si data.rightId est égal à 0
@@ -36,11 +32,9 @@ const b = parseUserData({
 
 const c = parseUserData({
   // retourner data.name ou "Anonymous" si data.name est falsey
-  //name: data.name,
   name: null,
 
   // convertir data.age en nombre, retourner 0 si ce n'est pas un nombre
-  //age: data.age,
   age: null,
 
   // retourner true si data.rightId est égal à 0
@@ -58,19 +52,19 @@ it("should parse name correctly", () => {
 });
 
 it("should parse age correctly", () => {
-  expect(a.age).to(28);
+  expect(a.age).toBe(28);
   expect(b.age).toBe(0);
   expect(c.age).toBe(0);
 });
 
 it("should parse isAdmin correctly", () => {
-  expect(a.isAdmin).to(true);
+  expect(a.isAdmin).toBe(true);
   expect(b.isAdmin).toBe(false);
   expect(c.isAdmin).toBe(false);
 });
 
 it("should parse friends correctly", () => {
-  expect(a.friends).to(3);
+  expect(a.friends).toBe(3);
   expect(b.friends).toBe(false);
   expect(c.friends).toBe(0);
 });
