97b34895f3ba7b32c9cc4a9e3fa05d605af5fd2f
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-09-18 14:27:07 +0200

    exo 10

---
+++ src/exo10.js
@@ -0,0 +1,6 @@
+// Utilisez l'opérateur spread et rest
+// compose(f,g,h)(x) <=> h(g(f(x)))
+
+export function compose() {
+  return;
+}
---
+++ test/exo10.test.js
@@ -0,0 +1,26 @@
+import { compose } from "../src/exo10";
+
+const join = (...args) => args.join(", "),
+  trim = s => s.trim(),
+  uppercase = s => s.toUpperCase(),
+  exclamation = s => s + "!";
+
+it("compose function with one argument", () => {
+  const f = compose(
+    uppercase,
+    exclamation
+  );
+  expect(f("hi")).toBe("HI!");
+});
+
+it("compose function with several arguments", () => {
+  const f = compose(
+    join,
+    trim,
+    uppercase,
+    exclamation
+  );
+
+  expect(f("Hello", "World").toBe("HELLO, WORLD!"));
+  expect(f("  one", "two", "three", "four  ").toBe("ONE, TWO, THREE, FOUR!"));
+});
