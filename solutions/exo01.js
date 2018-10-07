18656033ceacd201e3fa339b233c83baf7786e3f
Sylvain Pollet-Villard <sylvain.polletvillard@worldline.com>
2018-10-07 22:21:03 +0200

    style es5 pour l'exo 1

--- solutions/exo01.js
+++ solutions/exo01.js
@@ -1,7 +1,12 @@
 // retourne true si la variable passée est une primitive
 
 // solution 1
-export const isPrimitive = x => x === null || ['undefined', 'boolean', 'number', 'string', 'symbol'].includes(typeof x)
+export function isPrimitive(x) {
+    return x === null
+        || ['undefined', 'boolean', 'number', 'string', 'symbol'].includes(typeof x)
+}
 
 // solution 2
-export const isPrimitive2 = x => !x || !(x instanceof Object)
+export function isPrimitive2(x) {
+    return !x || !(x instanceof Object)
+}
