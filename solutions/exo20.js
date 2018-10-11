5c23b971b9baff9f41b727678f275753378fdbea
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-10-11 09:37:53 -0700

    solution exo 20

---
+++ solutions/exo20.js
@@ -0,0 +1,24 @@
+import data from "../test/exo20.data.json";
+
+export const query = array =>
+    Object.assign(array, {
+        where(field, condition) {
+            return query(this.filter(({ [field]: value }) => condition(value)))
+        },
+        orderBy(field) {
+            return query(this.sort((a, b) => a[field] < b[field] ? -1 : 1))
+        },
+        take(number) {
+            return query(this.slice(0, number))
+        }
+    });
+
+// exemple d'utilisation
+console.log(
+    query(data.users)
+        .where("age", n => n >= 18)
+        .orderBy("lastName")
+        .take(5)
+        .map(user => `${user.firstName} ${user.lastName}`)
+        .join("\n")
+);
