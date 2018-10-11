41ec1b06b35c07da0dd69304aa6673a6d462d6c8
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-10-11 09:36:07 -0700

    review exo 20

--- src/exo20.js
+++ src/exo20.js
@@ -3,15 +3,15 @@ import data from "../test/exo20.data.json";
 export const query = array =>
   Object.assign(array, {
     //TODO: implémenter les méthodes
-    where(filterFn) {},
-    orderBy(field) {},
-    take(number) {}
+    where(field, condition) { },
+    orderBy(field) { },
+    take(number) { }
   });
 
 // exemple d'utilisation
 console.log(
   query(data.users)
-    .where(user => user.age >= 18)
+    .where("age", n => n >= 18)
     .orderBy("lastName")
     .take(5)
     .map(user => `${user.firstName} ${user.lastName}`)
--- test/exo20.test.js
+++ test/exo20.test.js
@@ -3,8 +3,8 @@ import data from "./exo20.data.json";
 
 test("Query class", () => {
   const results = query(data.users)
-    .where(user => user.age >= 25)
-    .where(user => user.lastName.length > 5)
+    .where("age", n => n >= 25)
+    .where("lastName", name => name.length > 5)
     .orderBy("_id")
     .take(3)
     .map(user => user.company);
