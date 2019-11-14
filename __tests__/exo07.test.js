51070f4826808079d7a5f9272324f88cd3da15a9
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2019-11-14 17:25:33 +0100

    simplify exo07

--- __tests__/exo07.test.js
+++ __tests__/exo07.test.js
@@ -1,33 +1,29 @@
 import { getProductNames, getFruits, calcTotalPrice } from "../src/exo07";
 
-const data1 = {
-  products: [
-    { type: "legume", name: "Carotte", quantity: 2, unitPrice: 5 },
-    { type: "legume", name: "Chou", quantity: 3, unitPrice: 10 },
-    { type: "fruit", name: "Pomme", quantity: 1, unitPrice: 2 },
-    { type: "fruit", name: "Poire", quantity: 2, unitPrice: 3 }
-  ]
-};
+const products1 = [
+  { type: "legume", name: "Carotte", quantity: 2, unitPrice: 5 },
+  { type: "legume", name: "Chou", quantity: 3, unitPrice: 10 },
+  { type: "fruit", name: "Pomme", quantity: 1, unitPrice: 2 },
+  { type: "fruit", name: "Poire", quantity: 2, unitPrice: 3 }
+];
 
-const data2 = {
-  products: [
-    { type: "legume", name: "Carotte", quantity: 3, unitPrice: 1 },
-    { type: "legume", name: "Chou", quantity: 2, unitPrice: 4 },
-    { type: "legume", name: "Patates", quantity: 0, unitPrice: 1 }
-  ]
-};
+const products2 = [
+  { type: "legume", name: "Carotte", quantity: 3, unitPrice: 1 },
+  { type: "legume", name: "Chou", quantity: 2, unitPrice: 4 },
+  { type: "legume", name: "Patates", quantity: 0, unitPrice: 1 }
+]
 
 it("getProductNames", () => {
-  expect(getProductNames(data1).join(",")).toBe("Carotte,Chou,Pomme,Poire");
-  expect(getProductNames(data2).join(",")).toBe("Carotte,Chou,Patates");
+  expect(getProductNames(products1).join(",")).toBe("Carotte,Chou,Pomme,Poire");
+  expect(getProductNames(products2).join(",")).toBe("Carotte,Chou,Patates");
 });
 
 it("getFruits", () => {
-  expect(getFruits(data1).length).toBe(2);
-  expect(getFruits(data2).length).toBe(0);
+  expect(getFruits(products1).length).toBe(2);
+  expect(getFruits(products2).length).toBe(0);
 });
 
 it("calcTotalPrice", () => {
-  expect(calcTotalPrice(data1)).toBe(48);
-  expect(calcTotalPrice(data2)).toBe(11);
+  expect(calcTotalPrice(products1)).toBe(48);
+  expect(calcTotalPrice(products2)).toBe(11);
 });
--- src/exo07.js
+++ src/exo07.js
@@ -1,27 +1,25 @@
 // données d'exemple (d'autres sont utilisées en test)
-const data = {
-  products: [
-    { type: "legume", name: "Carotte", quantity: 3, unitPrice: 5 },
-    { type: "legume", name: "Chou", quantity: 1, unitPrice: 10 },
-    { type: "legume", name: "Patates", quantity: 10, unitPrice: 1 },
-    { type: "fruit", name: "Pomme", quantity: 6, unitPrice: 2 },
-    { type: "fruit", name: "Poire", quantity: 6, unitPrice: 3 }
-  ]
-};
+const products = [
+  { type: "legume", name: "Carotte", quantity: 3, unitPrice: 5 },
+  { type: "legume", name: "Chou", quantity: 1, unitPrice: 10 },
+  { type: "legume", name: "Patates", quantity: 10, unitPrice: 1 },
+  { type: "fruit", name: "Pomme", quantity: 6, unitPrice: 2 },
+  { type: "fruit", name: "Poire", quantity: 6, unitPrice: 3 }
+];
 
 // Résoudre l'exercice avec des arrow functions
 
-export function getProductNames(data) {
+export function getProductNames(products) {
   // retourne la liste des noms des produits (méthode map de Array)
   return [];
 }
 
-export function getFruits(data) {
+export function getFruits(products) {
   // retourne les fruits uniquement (méthode filter de Array)
   return [];
 }
 
-export function calcTotalPrice(data) {
+export function calcTotalPrice(products) {
   // retourne le prix total des produits (méthode reduce de Array)
   return 0;
 }
