c44c5afbbb2c8dee6da94a4983a8c0ba830739f2
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2019-11-15 14:01:54 +0100

    improve tests exo08

--- __tests__/exo08.test.js
+++ __tests__/exo08.test.js
@@ -2,10 +2,13 @@ import { count, mostFrequentIn } from "../src/exo08";
 
 test("count", () => {
   const countByArg = count("a", "b", "c", "c", "c", "b", "c", "a", "c", "b");
-  expect(Object.keys(countByArg).join(",")).toBe("a,b,c")
-  expect(Object.values(countByArg).join(",")).toBe("2,3,5")
+  expect(Object.keys(countByArg).sort().join(",")).toBe("a,b,c")
+  expect(countByArg['a']).toBe(2)
+  expect(countByArg['b']).toBe(3)
+  expect(countByArg['c']).toBe(5)
 })
 
 test("mostFrequentIn", () => {
-  expect(mostFrequentIn("a", "b", "c", "c", "c", "b", "c", "a", "c", "b")).toBe("c")
+  expect(mostFrequentIn("a", "b", "c", "a", "a", "b", "c", "a", "a", "b")).toBe("a")
+  expect(mostFrequentIn("a", "b", "b", "b", "a", "b", "b", "b", "a", "c")).toBe("b")
 })
