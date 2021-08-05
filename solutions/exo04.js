70196109f49e1f07613df25b161c1e94b4c68ce7
Sylvain Pollet-Villard <sylvain.polletvillard@worldline.com>
2021-08-05 17:23:59 +0200

    update solution exo 4

--- solutions/exo04.js
+++ solutions/exo04.js
@@ -1,16 +1,17 @@
-export function addWatcher(obj, prop, getter, setter) {
-  // change le descripteur de la propriété prop de l'objet obj pour appeler les fonctions:
-  // - getter lorsque la propriété prop est accédée
-  // - setter lorsque la propriété prop est modifiée
-  // il doit toujours être possible d'écrire et de lire une valeur pour la propriété prop
+export function watchProperty(obj, prop, onRead, onWrite) {
+  // changer le descripteur de la propriété prop de l'objet obj pour appeler les fonctions:
+  // - onRead() lorsque la propriété prop est accédée
+  // - onWrite(newValue) lorsque la propriété prop est modifiée
+  // ces fonctions sont juste des observateurs et n'ont pas de valeur de retour
+  // or il doit toujours être possible d'écrire et de lire une valeur pour la propriété prop
   let value = obj[prop];
   Object.defineProperty(obj, prop, {
     get() {
-      getter(value);
+      onRead(value);
       return value
     },
     set(newValue) {
-      setter(newValue)
+      onWrite(newValue)
       value = newValue;
     }
   })
