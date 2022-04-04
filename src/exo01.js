ffd81a3f7b4e19814deab7e2ccf0b6d3c03a5da2
faou1 <ouchene.faiza@yahoo.fr>
2022-04-04 12:42:22 +0200

    initial commit

--- src/exo01.js
+++ src/exo01.js
@@ -1,4 +1,7 @@
 // retourne true si la variable passée est une primitive
 export function isPrimitive(x) {
-    return true;
+  if ((x !== null && typeof x === "object") || typeof x === "function") {
+    return false;
+  }
+  return true;
 }
--- src/exo02.js
+++ src/exo02.js
@@ -1,14 +1,18 @@
 // retourne true si l'objet a une propriété ayant comme clé key
 export function hasProperty(obj, key) {
-  return true;
+  return obj && key in obj;
 }
 
 // retourne true si l'objet a une propriété ayant comme valeur value
 export function hasPropertyValue(obj, value) {
-  return true;
+  if (!obj) return false;
+  for (let key in obj) {
+    if (obj[key] === value) return true;
+  }
+  return false;
 }
 
 // retourne le nombre de propriétés d'un objet (note: sans compter les propriétés déléguées)
 export function getNumberOfProperties(obj) {
-  return 0;
+  return obj && Object.keys(obj).length;
 }
