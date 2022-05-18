f70e419868e77eff7e0dc95f5793ff2bbb58b059
Sylvain Pollet-Villard <sylvain.polletvillard@worldline.com>
2022-05-18 18:36:03 +0200

    update solution exo 4

--- solutions/exo04.js
+++ solutions/exo04.js
@@ -1,8 +1,8 @@
-export function watchProperty(obj, prop, onRead, onWrite) {
-  // changer le descripteur de la propriété prop de l'objet obj pour appeler les fonctions:
-  // - onRead() lorsque la propriété prop est accédée
-  // - onWrite(newValue) lorsque la propriété prop est modifiée
-  // ces fonctions sont juste des observateurs et n'ont pas de valeur de retour
+export function spyOnProperty(obj, prop, onRead, onWrite) {
+  // changer le descripteur de la propriété prop de l'objet obj:
+  // - appeler onRead() lorsque la propriété prop est accédée
+  // - appeler onWrite(newValue) lorsque la propriété prop est modifiée
+  // ces fonctions sont juste des observateurs, elles n'ont pas de valeur de retour
   // or il doit toujours être possible d'écrire et de lire une valeur pour la propriété prop
   let value = obj[prop];
   Object.defineProperty(obj, prop, {
@@ -19,13 +19,13 @@ export function watchProperty(obj, prop, onRead, onWrite) {
 
 export function setPrivatesAndConstants(obj) {
   // changer le descripteur de chaque propriété de l'objet:
-  // énumérable si la clé ne commence pas par par un _
-  // mutable et configurable si la clé ne commence pas par une majuscule (A-Z)
+  // les propriétés dont la clé commence par un _ doivent être non énumérables
+  // les propriétés dont la clé commence par une majuscule doivent être non modifiables et non configurables
   for (let prop in obj) {
     Object.defineProperty(obj, prop, {
       enumerable: !prop.startsWith("_"),
-      writable: !/[A-Z]/.test(prop[0]),
-      configurable: !/[A-Z]/.test(prop[0])
+      writable: prop[0].toUpperCase() !== prop[0],
+      configurable: !/[A-Z]/.test(prop[0]) // alternative pour tester la majuscule
     })
   }
 
