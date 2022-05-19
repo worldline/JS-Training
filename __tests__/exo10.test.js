4f698fca231a8d7a5e6df7ed66aebd8abeea4a40
Sylvain Pollet-Villard <sylvain.polletvillard@worldline.com>
2022-05-19 19:16:13 +0200

    fix solution exo 10

--- __tests__/exo10.test.js
+++ __tests__/exo10.test.js
@@ -4,15 +4,7 @@ const name = "Jean",
   nbMails = 5;
 
 it("should translate in fr", () => {
-  i18n.locale = "fr";
   expect(i18n`Hello ${name}, you have ${nbMails} new mails.`).toBe(
     "Bonjour Jean, vous avez 5 nouveaux messages."
-  );
-});
-
-it("should keep strings in english", () => {
-  i18n.locale = "en";
-  expect(i18n`Hello ${name}, you have ${nbMails} new mails.`).toBe(
-    "Hello Jean, you have 5 new mails."
   );
 });
--- solutions/exo10.js
+++ solutions/exo10.js
@@ -1,7 +1,7 @@
 export function i18n(strings, ...vars) {
   // solution 1
   return strings.reduce(
-    (out, str, i) => `${out}${i18n.translate(str)}${vars[i] || ""}`,
+    (out, str, i) => `${out}${translations[str]}${vars[i] || ""}`,
     ""
   )
 }
@@ -9,24 +9,15 @@ export function i18n(strings, ...vars) {
 export function i18n2(strings, ...vars) {
   // solution 2
   return strings
-    .map((str, i) => i18n.translate(str) + (vars[i] || ""))
+    .map((str, i) => translations[str] + (vars[i] || ""))
     .join("");
 }
 
-Object.assign(i18n, {
-  locale: "fr",
-  locales: {
-    fr: {
-      "Hello ": "Bonjour ",
-      ", you have ": ", vous avez ",
-      " new mails.": " nouveaux messages."
-    }
-  },
-  translate(str) {
-    let messages = i18n.locales[i18n.locale] || {};
-    return messages[str] || str;
-  }
-});
+const translations = {
+  "Hello ": "Bonjour ",
+  ", you have ": ", vous avez ",
+  " new mails.": " nouveaux messages."
+}
 
 const name = "Bob",
   nbMails = 3;
