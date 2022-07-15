fe293c826e72894f9a37a73f7dc6808be3430507
Nogtif <quentin.cptr@gmail.com>
2022-07-15 15:02:10 +0200

    🦓 review starting reveal

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// More info about initialization & config:
+// - https://revealjs.com/initialization/
+// - https://revealjs.com/config/
+Reveal.initialize({
+  history: true,
+  transition: 'linear',
+  hash: true,
+
+  // Learn about plugins: https://revealjs.com/plugins/
+  plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+});
