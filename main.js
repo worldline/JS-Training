e6a472493877696999d92a506f8d56b7e6ede1e7
Nogtif <quentin.cptr@gmail.com>
2022-07-19 11:05:30 +0200

    ⚡ use vite server

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// More info about initialization & config:
-// - https://revealjs.com/initialization/
-// - https://revealjs.com/config/
-Reveal.initialize({
+import Reveal from 'reveal.js'
+
+const deck = new Reveal()
+deck.initialize({  
   history: true,
   transition: 'linear',
   hash: true,
 
   // Learn about plugins: https://revealjs.com/plugins/
   plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
-});
+})
