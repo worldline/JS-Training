32b30829269ecc9258eb829e96033f5dac5dfb99
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-10-01 08:57:39 +0200

    alternative solution for exo18

--- solutions/exo18.js
+++ solutions/exo18.js
@@ -11,3 +11,20 @@ export const range = (start, end) => {
 // example:
 // [...range(5,10)]
 // -> [5,6,7,8,9,10]
+
+export const range2 = (start, end) => {
+    // return an iterable iterating over all the numbers between two bounds
+    return {
+        [Symbol.iterator](){
+            let i = start
+            return {
+                next(){
+                    return {
+                        done: i >= end,
+                        value: i++
+                    }
+                }
+            }
+        }
+    }
+};
